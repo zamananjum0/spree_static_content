53c1d028e6516549aeec379ed47b31269d97bc69
Tobias Bohwalli <hi@futhr.io>
2014-12-29 13:10:09 +0100

    Move admin navbar link for pages.
    It does not belongs in Configuration so let give it its own tab similar
    to Users without sub menus. Also add cancan support for menu link.

--- app/overrides/add_pages_to_admin_configuration_sidebar.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path: 'spree/layouts/admin',
-  name: 'pages_admin_configurations_menu',
-  insert_bottom: '#main-sidebar',
-  partial: 'spree/admin/shared/pages_sidebar_menu'
-)
