af2761e401ab64ef69b67a1c2f47afe02e60c349
Peter Berkenbosch <peter@pero-ict.nl>
2015-02-20 18:09:51 +0100

    fix the controller specs updates for bootstrap admin

--- app/overrides/add_pages_to_admin_configuration_sidebar.rb
+++ app/overrides/add_pages_to_admin_configuration_sidebar.rb
@@ -1,5 +1,6 @@
-Deface::Override.new(virtual_path:  "spree/admin/shared/_configuration_menu",
-                     name:          "pages_admin_configurations_menu",
-                     insert_bottom: "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
-                     text:          "<%= configurations_sidebar_menu_item Spree.t(:pages), admin_pages_path %>",
-                     disabled:      false)
+Deface::Override.new(
+  virtual_path: 'spree/layouts/admin',
+  name: 'pages_admin_configurations_menu',
+  insert_bottom: '#main-sidebar',
+  partial: 'spree/admin/shared/pages_sidebar_menu'
+)
