f4b055ddf8debb345b8496edec9189b946bb9787
odk211 <tomoki.odaka@gmail.com>
2015-09-16 11:48:14 +0900

    fix deface warning

--- lib/spree_static_content/engine.rb
+++ lib/spree_static_content/engine.rb
@@ -7,9 +7,6 @@ module SpreeStaticContent
     config.autoload_paths += %W(#{config.root}/lib)
 
     def self.activate
-      Dir.glob(%W(#{config.root}/app/overrides/*.rb)) do |klass|
-        Rails.configuration.cache_classes ? require(klass) : load(klass)
-      end
     end
 
     config.to_prepare(&method(:activate).to_proc)
